55545c16f85f550c7073aa100e7b2c043100370e
wudi <wudi19870903@163.com>
2017-03-31 10:11:03 +0800

    x

--- StormStock/src/stormstock/ori/capi/TestCATHSAccount.java
+++ StormStock/src/stormstock/ori/capi/TestCATHSAccount.java
@@ -1,6 +1,8 @@
 package stormstock.ori.capi;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Formatter;
 import java.util.List;
 
@@ -57,14 +59,52 @@ public class TestCATHSAccount {
         
 		// ÂòÈëÎ¯ÍÐ²âÊÔ
 		{
-	        int iBuyRet = CATHSAccount.buyStock("601988", 100, 0.07f);
-	        fmt.format("CATHSAccount.buyStock err(%d)\n", iBuyRet);
+	        //int iBuyRet = CATHSAccount.buyStock("601988", 100, 0.07f);
+	        //fmt.format("CATHSAccount.buyStock err(%d)\n", iBuyRet);
 		}
         
 		// Âô³öÎ¯ÍÐ²âÊÔ
 		{
-	        int iSellRet = CATHSAccount.sellStock("601988", 100, 190.7f);
-	        fmt.format("CATHSAccount.sellStock err(%d)\n", iSellRet);
+	        //int iSellRet = CATHSAccount.sellStock("601988", 100, 190.7f);
+	        //fmt.format("CATHSAccount.sellStock err(%d)\n", iSellRet);
+		}
+		
+		// test loop
+		{
+			long testtimes = Long.MAX_VALUE;
+			for(int m=0; m<testtimes; m++)
+			{
+				
+				SimpleDateFormat sdf =new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+				String datetime = sdf.format(new Date());
+				
+				fmt.format("\n[%s] CATHSAccount testing times(%s) ==================>>>>\n", datetime, m);
+				
+				ResultAvailableMoney cResultAvailableMoney = CATHSAccount.getAvailableMoney();
+		        fmt.format("    CATHSAccount.getAvailableMoney err(%d) AvailableMoney(%.2f)\n", cResultAvailableMoney.error, cResultAvailableMoney.availableMoney);
+		        
+		        ResultTotalAssets cResultTotalAssets = CATHSAccount.getTotalAssets();
+		        fmt.format("    CATHSAccount.getTotalAssets err(%d) TotalAssets(%.2f)\n", cResultTotalAssets.error, cResultTotalAssets.totalAssets);
+		        
+		        ResultAllStockMarketValue cResultAllStockMarketValue = CATHSAccount.getAllStockMarketValue();
+		        fmt.format("    CATHSAccount.getAllStockMarketValue err(%d) AllStockMarketValue(%.2f)\n", cResultAllStockMarketValue.error, cResultAllStockMarketValue.allStockMarketValue);
+		        
+		        ResultHoldStockList cResultHoldStockList = CATHSAccount.getHoldStockList();
+		        fmt.format("    CATHSAccount.getHoldStockList err(%d) resultList size(%d)\n", cResultHoldStockList.error, cResultHoldStockList.resultList.size());
+		        for(int i=0;i<cResultHoldStockList.resultList.size();i++)
+		        {
+		        	HoldStock cHoldStock = cResultHoldStockList.resultList.get(i);
+		        	fmt.format("        {%s %d %d %.3f %.3f %.3f}\n", cHoldStock.stockID, cHoldStock.totalAmount, cHoldStock.availableAmount,
+		        			cHoldStock.refProfitLoss, cHoldStock.refPrimeCostPrice, cHoldStock.curPrice);
+		        }
+		        
+				try {
+					Thread.sleep(1000*60*5);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 		}
 
         fmt.format("### main end\n");
